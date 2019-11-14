a6f70db6ed892b289308241025427f5c40906d65
zhangning <zhangning@momenta.ai>
2019-11-14 13:02:13 +0800

    init

---
+++ opencv/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zn/opecv_code/opencv/include
+# Build directory: /home/zn/opecv_code/opencv/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ opencv/sample/1.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: zhangning
+	> Mail: amoscykl@163.com 
+	> Created Time: 2019年11月13日 星期三 19时56分08秒
+ ************************************************************************/
+
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace std;
+
+
+int main(int a,char **argv)
+{
+
+	cv::Mat image;
+	image=cv::imread(argv[1]);
+	cv::imshow("image",image);
+	cv::waitKey();
+}
+
+
